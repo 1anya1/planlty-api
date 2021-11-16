53303c1365a6fad15f5f13ba197f7b9765a1c3e0
Anna Filatova <filatovaa@yahoo.com>
2021-11-16 09:15:17 -0800

    addition to cors.rd

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'http://localhost:3000', 'https://plantly-app.netlify.app'
+      origins 'http://localhost:3000', 'https://plantly-app.netlify.app', 'https://plantly-app.herokuapp.com/'
     
       resource '*',
         headers: :any,
